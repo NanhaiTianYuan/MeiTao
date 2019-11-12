460126e514a89f9ebe95cfd5396e593367c10ac1
Dave <1497942221@qq.com>
2019-11-12 09:30:00 +0800

    player123456

---
+++ palyer01/src/org/java/text/Text.java
@@ -0,0 +1,15 @@
+package org.java.text;
+
+public class Text {
+    public static void main(String[] args) {
+        System.out.println("Hellow World!");
+        int i=2;
+        System.out.println("i = " + i);
+        System.err.println("如果的事");
+       if(i==2){
+           System.out.println("i = " + i);
+           i++;
+           System.out.println("i = " + i);
+       }
+    }
+}
